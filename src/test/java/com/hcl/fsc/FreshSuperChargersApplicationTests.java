ec0fc0000e31ff2f12e1e88efbf41df75bfea39f
amankumar-srivastava <132335189+amankumar-srivastava@users.noreply.github.com>
2023-10-09 12:53:27 +0530

    Update FreshSuperChargersApplicationTests.java

--- src/test/java/com/hcl/fsc/FreshSuperChargersApplicationTests.java
+++ src/test/java/com/hcl/fsc/FreshSuperChargersApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FreshSuperChargersApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
